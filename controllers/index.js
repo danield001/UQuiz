141483964fe2cef253c53e28005a892fb6285964
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-12-18 22:28:24 +1030

    debug paths

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { Model } = require('sequelize');
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
