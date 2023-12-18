f3428e0881ae80eea8a488bd99804e0c61acebd8
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-12-18 20:39:47 +1030

    debugged Models before question_id to JSON

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,24 @@
 const sequelize = require('../config/connection');
-const { Category, Question, QuestionTag, Tag, User, UsedQuestions } = require('../models');
+const { Category, Question, Quiz, User } = require('../models');
 
 const categorySeedData = require('./categorySeedData.json');
 const questionSeedData = require('./questionSeedData.json');
-const tagSeedData = require('./tagSeedData.json');
 const userSeedData = require('./userSeedData');
+const quizSeedData = require('./quizSeedData.json');
 
 const seedDatabase = async () => {
   try {
     await sequelize.sync({ force: true });
 
+    const user = await User.bulkCreate(userSeedData);
+    console.log('Users seeded successfully.');
     const categories = await Category.bulkCreate(categorySeedData);
-    console.log('Categories seeded suxxessfully.');
+    console.log('Categories seeded successfully.');
     const questions = await Question.bulkCreate(questionSeedData);
     console.log('Questions seeded successfully.');
-    // const tags = await Tag.bulkCreate(tagSeedData);
+    const quiz = await Quiz.bulkCreate(quizSeedData);
+    console.log('Quizzes seeded successfully.');
+    
   
     // const users = await User.bulkCreate(userSeedData);
     console.log('Database seeded successfully.');
