b034901a7bb05649af94117c0f2c62a54d230fd3
danield001 <30894506+danield001@users.noreply.github.com>
2023-12-21 16:33:09 +0800

    changes to routers in homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,6 +7,18 @@ router.get("/example", (req, res)=>{
   res.render("example")
 })
 
+router.get("/login", (req, res)=>{
+  res.render("login")
+})
+
+router.get("/quiz", (req, res)=>{
+  res.render("quiz")
+})
+
+router.get("/homepage", (req, res)=>{
+  res.render("homepage")
+})
+
 
 module.exports = router;
 
