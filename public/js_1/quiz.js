2b653dd6c3f99623e988da04fc1529428d1d214d
Amy Lloyd <amylloyd30@gmail.com>
2023-12-26 08:52:46 +1030

    add radio values

--- public/js_1/quiz.js
+++ public/js_1/quiz.js
@@ -6,6 +6,7 @@ const gameOverScreen = document.getElementById("game-over-screen");
 //set variables to access buttons including choices buttons
 const startButton = document.getElementById("start-button");
 const nextButton = document.getElementById("next-button");
+const submitButton = document.getElementById("submit-button");
 
 //set original attributes of sections
 gameOverScreen.setAttribute("style", "visibility: hidden;");
@@ -20,6 +21,11 @@ const choiceD = document.getElementById("choice-d");
 const questionBody = document.getElementById("question-body");
 const questionCreator = document.getElementById("question-creator");
 const answerTextEl = document.getElementById("check-answer");
+
+const radioValA = document.getElementById("radio-value-a");
+const radioValB = document.getElementById("radio-value-b");
+const radioValC = document.getElementById("radio-value-c");
+const radioValD = document.getElementById("radio-value-d");
 
 let questions = [];
 //handler to dynamically render the pulled database information on the page. 
@@ -83,6 +89,10 @@ const renderQuestion = () => {
     choiceC.textContent = currentQuestion.choice_c;
     choiceD.textContent = currentQuestion.choice_d;
 
+    radioValA.setAttribute("value", `${currentQuestion.choice_a}`);
+    radioValB.setAttribute("value", `${currentQuestion.choice_b}`);
+    radioValC.setAttribute("value", `${currentQuestion.choice_c}`);
+    radioValD.setAttribute("value", `${currentQuestion.choice_d}`);
 };
 
 const nextButtonHandler = (event) => {
@@ -101,9 +111,23 @@ const nextButtonHandler = (event) => {
     }
 }
 
+//create variable to hold score
+let score = 0;
+
+//add event listener to submit choice
+const submitButtonHandler = (event) => {
+
+    event.preventDefault();
+
+
+
+}
 
 // document.querySelector('#submit-choice').addEventListener('click', submitChoice);
 document.querySelector('#start-button').addEventListener('click', startButtonHandler);
 
-//Handler for the submitChoice answer on quiz-page
+//Handler for the next button on quiz-page
 document.querySelector('#next-button').addEventListener('click', nextButtonHandler);
+
+//Handler for the submitChoice answer on quiz-page
+document.querySelector('#submit-button').addEventListener('click', submitButtonHandler);
