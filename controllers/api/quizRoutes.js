6b2e91f6f9177bd07c45b8d86227539cebd84c13
Amy Lloyd <amylloyd30@gmail.com>
2023-12-30 01:14:49 +1030

    WIP rendering data values from controller for form

--- controllers/api/quizRoutes.js
+++ controllers/api/quizRoutes.js
@@ -1,44 +1,37 @@
 const router = require('express').Router();
-const { Question, Quiz, QuizQuestion } = require('../../models');
+const { Question, Quiz, QuizQuestion, Category } = require('../../models');
 
-// //GET request at this route: http://localhost:3001/api/quizzes
-// router.get('/', async (req, res) => {
-//     try {
-//         //Get all quizzes and JOIN with question data
-//         const dbQuizData = await Quiz.findAll({
-//             include: [
-//                 {
-//                     model: Question,
-//                     as: 'questions',
-//                     attributes: [
-//                         'id',
-//                         'question_body',
-//                         'choice_a',
-//                         'choice_b',
-//                         'choice_c',
-//                         'choice_d',
-//                         'answer',
-//                         'created_by_user_id'
-//                     ],
-//                 },
-//             ],
-//         });
-        
-//         const quizzes = await dbQuizData.map((quiz) =>
-//         {
-//         console.log(quizzes);
-//         quiz.get({ plain: true })
-//         });
-//         console.log(quizzes);
-//         res.render("quiz", {
-//             quizzes
-//         });
+//GET request at this route  /api/quiz'
+router.get('/', async (req, res) => {
+    try {
+        //Get all quizzes and JOIN with question data
+        const dbQuizData = await Quiz.findAll({
+            include: [
+                {
+                    model: Question,
+                    as: 'questions',
+                    attributes: [
+                        'id',
+                        'question_body',
+                        'category_id',
+                        'created_by_user_id'
+                    ],
+                }
+            ],
+        });
+        // Serialize data so the template can read it
+        const quizzes = dbQuizData.map((quiz) => quiz.get({ plain:true }));
 
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal Server Error', details: err.message });
-//     }
-// });
+        // // Pass serialized data and session flag into template
+        res.render("quiz", {
+            quizzes,
+            // logged_in: req.session.logged_in 
+        });
+
+    } catch (err) {
+        res.status(500).json({ error: 'Internal Server Error', details: err.message });
+    }
+});
 
 
 
