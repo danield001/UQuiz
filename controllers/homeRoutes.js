12a39c51364c5da4fde58ab0bc09115337ebafd6
danield001 <30894506+danield001@users.noreply.github.com>
2023-12-29 23:38:00 +1030

    partial changes to login and account to keep session active - currently partially complete

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,19 @@
 const router = require('express').Router();
+const express = require('express')
+const path = require('path')
 
 const withAuth = require('../utils/auth');
 const { Question, Quiz, QuizQuestion } = require('../models/index');
 
-router.get("/example", (req, res)=>{
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+router.get("/example", (req, res) => {
   res.render("example")
 })
 
-router.get("/login", (req, res)=>{
+router.get("/login", (req, res) => {
   res.render("login")
 })
 
@@ -15,47 +21,60 @@ router.get("/login", (req, res)=>{
 //   res.render("quiz")
 // })
 
-router.get("/homepage", (req, res)=>{
+router.get("/", (req, res) => {
   res.render("homepage")
+})
+
+router.get("/account", (req, res) => {
+  res.render("account")
 })
 
 //GET request at this route: http://localhost:3001/quiz/:id
 //get request to render the page
 router.get("/quiz/:id", async (req, res) => {
   try {
-      const dbQuizData = await Quiz.findByPk(req.params.id, {
-          include: [
-              {
-                  model: Question,
-                  through: QuizQuestion,
-                  as: "questions",
-                  attributes: [
-                      "id",
-                      "question_body",
-                      "choice_a",
-                      "choice_b",
-                      "choice_c",
-                      "choice_d",
-                      "answer",
-                      "created_by_user_id",
-                  ],
-              },
+    const dbQuizData = await Quiz.findByPk(req.params.id, {
+      include: [
+        {
+          model: Question,
+          through: QuizQuestion,
+          as: "questions",
+          attributes: [
+            "id",
+            "question_body",
+            "choice_a",
+            "choice_b",
+            "choice_c",
+            "choice_d",
+            "answer",
+            "created_by_user_id",
           ],
-      });
+        },
+      ],
+    });
 
-      const quizPage = dbQuizData.get({ plain: true });
+    const quizPage = dbQuizData.get({ plain: true });
 
-      console.log("Rendering quiz page:", quizPage);
-      
-      res.render("quiz-page", { 
-          quizPage 
-      });       
-      
+    console.log("Rendering quiz page:", quizPage);
+
+    res.render("quiz-page", {
+      quizPage
+    });
+
   } catch (err) {
-      console.error(err);
-      res.status(500).json({ err: 'Internal Server Error', details: err.message });
+    console.error(err);
+    res.status(500).json({ err: 'Internal Server Error', details: err.message });
   }
 });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
 
 module.exports = router;
 
